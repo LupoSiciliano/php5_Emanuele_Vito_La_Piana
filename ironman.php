4e9375d6313957c635c1c4570fc81dafe80cbb98
Emanuele La Piana <emanu360hotmail.it>
2024-04-17 18:29:13 +0200

    INIZIO IRONMAN

--- ironman.php
+++ ironman.php
@@ -7,5 +7,13 @@
 // aggiungi un contatore di quanti Ironman hai creato -->
 
 abstract class Armor {
-    
+    Traccia 3: 
+
+
+
+Crea la tua armatura di Ironman seguendo il modello visto a lezione, seguendo i seguenti punti: 
+
+metti ogni classe (abstract o no) in un file diverso, utilizzando nel file principale la funzione require() 
+crea un esercito random di Ironman 
+aggiungi un contatore di quanti Ironman hai creato
 }
